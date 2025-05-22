a1ac28ef1ad3de50256c7db09b18f8afbdb6f533
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-05-21 21:01:33 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int numero, resultado;
+
+Console.Write("Digite um número: ");
+numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Tabuada do {0}", numero);
+
+for (int i = 0; i < 10; i++) {
+    resultado = numero * i;
+
+    Console.WriteLine("{0} x {1} = {2}", numero, i, resultado);
+}
